3b72d6212a22566d43a339fb9281b804951f440f
nimaid <ellagjameson@gmail.com>
2020-07-02 23:48:24 -0700

    Duplicate line removed

--- nested-pdf-merge.py
+++ nested-pdf-merge.py
@@ -126,7 +126,6 @@ def iterdict(d, base_path=""):
                 bm_name = args["order_number_seperator"].join(k.split(args["order_number_seperator"])[1:]).strip(" ")
             print(ident + bm_name + "\tPage #" + str(last_page_index + 1))
             ident += ident_str
-            ident += ident_str
             
             path_list.append(k)
             
